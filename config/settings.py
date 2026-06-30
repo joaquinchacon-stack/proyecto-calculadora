58328d12d6d56ab2df077578bdd5dfa8f7cbd0b6
joaquinchacon-stack <joaquin.chacon@fupbi.com>
2026-06-30 03:53:39 +0000

    actualizacion nueva 6

--- config/settings.py
+++ config/settings.py
@@ -78,15 +78,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'calculadora_db',
-        'USER': 'root',
-        'PASSWORD': 'root',  # En Codespaces por defecto el usuario root local no tiene contraseña
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / 'db.sqlite3',
     }
 }
 
